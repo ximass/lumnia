d1dbbc19f95f9aed1cab1ac12d26bb8d3097ac3c
ximass <mateus.schmitz7@hotmail.com>
2025-10-13 19:26:43 -0300

    fix: csrf token on login

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/register',
+            'api/login',
+            'api/logout',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->report(function (Throwable $e) {
